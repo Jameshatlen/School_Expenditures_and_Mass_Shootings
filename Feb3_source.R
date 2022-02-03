78bfcb04e2e9f32cc6eec5922e781acc767d3c02
Jameshatlen <89991488+Jameshatlen@users.noreply.github.com>
2022-02-03 15:25:11 -0600

    current leaflet draft feb 3 and pop_names is the popup_2015 and popup_2016 vectors that are the expenditures of states on education for those years

---
+++ Feb3_source.R
@@ -0,0 +1,50 @@
+leaflet() %>%
+addProviderTiles("CartoDB.Positron") %>%
+setView(-98.483330, 38.712046, zoom = 3.5) %>%
+addPolygons(data = state_w_expenditures_na_num ,
+fillColor = ~fillcolor_expenditures(state_w_expenditures_na_num$X2015),
+fillOpacity = 1,
+stroke=TRUE,
+weight = .2,
+color = "#8f29ac",
+smoothFactor = 0.2,
+highlight = highlightOptions(
+weight = 5,
+color = "#555",
+fillOpacity = 0.8,
+bringToFront = FALSE),
+label = ~popup_2015_names,
+labelOptions = labelOptions(
+style = list("font-weight" = "normal", padding = "3px 8px"),
+textsize = "15px",
+direction = "auto"),
+group = "2015")  %>%
+addPolygons(data = state_w_expenditures_na_num ,
+fillColor = ~fillcolor_expenditures(state_w_expenditures_na_num$X2016),
+fillOpacity = 1,
+stroke=FALSE,
+weight = 0.2,
+smoothFactor = 0.2,
+label = ~popup_2016_names ,
+group = "2016")  %>%
+addCircleMarkers(lng = shooting_df_2014_replaced[173:237,"Longitude"] ,
+lat = shooting_df_2014_replaced[173:237,"Latitude"],
+radius = 2,
+label = shooting_df_2014_replaced[173:237,"Title"],
+color = "#651689",
+group = "2015") %>%
+addCircleMarkers(lng = shooting_df_2016_lon ,
+lat = shooting_df_2016_lat,
+radius = 2,
+popup = shooting_df_2016_title,
+color = "#000000",
+group = "2016") %>%
+addLayersControl(baseGroups = c("2015","2016")) %>%
+addLegend(pal = fillcolor_expenditures,
+values = state_w_expenditures_na_num$X2016,
+position = "bottomleft",
+title = "Expenditures",
+group = "2016")
+
+
+#above is latest
