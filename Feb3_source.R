d9c98483b537709e66cc9779125d865e412da302
Jameshatlen <89991488+Jameshatlen@users.noreply.github.com>
2022-02-03 19:40:21 -0600

    Feb3_source.R has the code for a map that is done besides fixing the state labels and fixing the legend OR adding a title box

--- Feb3_source.R
+++ Feb3_source.R
@@ -49,3 +49,73 @@ group = "2016")
 
 #above is latest
 #html doesnt work for labels, only popups (not fixed yet)
+
+leaflet() %>%
+addProviderTiles("CartoDB.Positron") %>%
+setView(-98.483330, 38.712046, zoom = 3.5) %>%
+addPolygons(data = state_w_expenditures_na_num ,
+fillColor = ~fillcolor_expenditures(state_w_expenditures_na_num$X2015),
+fillOpacity = 1,
+stroke= FALSE,
+weight = .5,
+color = "#8f29ac",
+smoothFactor = 0,
+highlight = highlightOptions(
+weight = 2,
+color = "#b600e3",
+fillOpacity = 0.4,
+bringToFront = FALSE),
+label = ~popup_2015_names,
+labelOptions = labelOptions(
+style = list("font-weight" = "normal", padding = "3px 8px"),
+textsize = "15px",
+direction = "auto"),
+group = "2015")  %>%
+addPolygons(data = state_w_expenditures_na_num ,
+fillColor = ~fillcolor_expenditures(state_w_expenditures_na_num$X2016),
+fillOpacity = 1,
+stroke=FALSE,
+weight = 0,
+smoothFactor = 0,
+highlight = highlightOptions(
+weight = 2,
+color = "#b600e3",
+fillOpacity = 0.4,
+bringToFront = FALSE),
+label = ~popup_2016_names,
+labelOptions = labelOptions(
+style = list("font-weight" = "normal", padding = "3px 8px"),
+textsize = "15px",
+direction = "auto"),
+group = "2016")  %>%
+addCircleMarkers(lng = shooting_df_2014_replaced[173:237,"Longitude"] ,
+lat = shooting_df_2014_replaced[173:237,"Latitude"],
+radius = 2,
+label = shooting_df_2014_replaced[173:237,"Title"],
+color = "#000000",
+opacity = 1,
+group = "2015") %>%
+addCircleMarkers(lng = shooting_df_2016_lon ,
+lat = shooting_df_2016_lat,
+radius = 2,
+popup = shooting_df_2016_title,
+color = "#000000",
+opacity = 1,
+group = "2016") %>%
+addLayersControl(baseGroups = c("2015","2016"),
+options = layersControlOptions(collapsed = FALSE)) %>%
+addLegend(pal = fillcolor_expenditures,
+values = state_w_expenditures_na_num$X2016,
+labFormat = labelFormat(prefix = "$"),
+position = "bottomleft",
+title = "Expenditures per Pupil",
+group = "2016")
+
+
+#above needs floating labels fixed, and legend fixed, perhaps by removing na items
+#fixing labels:
+#https://www.r-graph-gallery.com/183-choropleth-map-with-leaflet.html
+#https://rstudio.github.io/leaflet/choropleths.html
+
+#title addition possile:
+#https://marcinstepniak.eu/post/interactive-choropleth-maps-with-r-and-tmap-part-i/
--- README.md
+++ README.md
@@ -17,6 +17,10 @@ https://www.kaggle.com/jlmontie/stanford-msa-2017?select=mass_shooting_events_st
 
 School Expenditures per Pupil - National Science Foundation
 https://ncses.nsf.gov/indicators/states/indicator/public-school-per-pupil-expenditures
+Expenditures per Pupil for Elementary and Secondary Public Schools
+
+This indicator represents the amount that local, state, and federal governments spend on elementary and secondary education, adjusted for the size of the student body. It is calculated by dividing the current expenditures over the entire academic year for prekindergarten through grade 12 by the number of students in those grades in public schools. Current expenditures include instruction and instruction-related costs, student support services, administration, and operations and exclude funds for school construction and other capital outlays, debt service, and programs outside of public elementary and secondary education.
+
 
 
 Leaflet tutorials:
